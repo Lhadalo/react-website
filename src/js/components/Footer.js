9cfc06c5a44fa60c24ffe704e1afe3ece396bf0d
Ola Dahl <oladahl.lel@gmail.com>
2018-12-31 00:18:23 +0100

    Add footer

---
+++ src/js/components/Footer.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import Box from 'grommet/components/Box';
+
+import Anchor from 'grommet/components/Anchor';
+
+import LinkedIn from 'grommet/components/icons/base/SocialLinkedinOption';
+import Github from 'grommet/components/icons/base/SocialGithub';
+import Mail from 'grommet/components/icons/base/Mail';
+
+const Footer = () => (
+	<Box align='center' separator='top' margin={{ horizontal: 'medium' }}>
+		<Box direction='row' margin='small'>
+			<Anchor icon={<Mail />} />
+			<Anchor icon={<LinkedIn />} />
+			<Anchor icon={<Github />} />
+		</Box>
+	</Box>
+);
+
+export default Footer;
